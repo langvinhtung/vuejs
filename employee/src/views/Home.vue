f178ef3fab1dfff0309d058d3fd302435cd1f5f9
DESKTOP-2CERFE2\Admin <lvtung1@cmc.com.vn>
2020-04-19 14:41:50 +0700

    commit 3

--- employee/src/views/Home.vue
+++ employee/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-form @submit.prevent.stop="onSubmit" action>
+      <label for="text-password">User Name</label>
+      <b-input
+        v-model="user.userName"
+        type="text"
+        id="text-password"
+        aria-describedby="password-help-block"
+      ></b-input>
+      <label for="text-password">Password</label>
+      <b-input
+        v-model="user.password"
+        type="password"
+        id="text-password"
+        aria-describedby="password-help-block"
+      ></b-input>
+      <br>
+
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      user: {
+        userName: "",
+        password: ""
+      },
+      userLogin: {
+        userName: "lvtung1",
+        password: "12345678a@"
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      if (
+        this.user.userName == this.userLogin.userName &&
+        this.user.password == this.userLogin.password
+      ) {
+        this.$router.push({
+          path: "/about",
+          name: "About"
+        });
+      } else {
+        alert("Loi")
+      }
+    }
   }
-}
+};
 </script>
